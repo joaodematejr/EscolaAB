19bdaf385993f546a0479948c3e46044a46a7895
Joao <joao_edemar@estudante.sc.senai.br>
2016-05-18 15:08:56 -0300

    Atualizaçao da Classe Estado/ Versão estavel

--- src/entity/Estado.java
+++ src/entity/Estado.java
@@ -1,6 +1,6 @@
 package entity;
 
 public enum Estado {
-	AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO,
+	PARANA, SANTACATARINA, RIOGRANDEDOSUL
 
 }
